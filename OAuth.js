97911db8483ed72a46de10e84df9297aeaa1f958
kesti-song <tysong@kesti.co.kr>
2020-05-01 20:18:42 +0900

    first commit test

---
+++ OAuth.js
@@ -0,0 +1,18 @@
+// Load client library.
+var ee = require('@google/earthengine');
+
+// Initialize client library and run analysis.
+var initialize = function() {
+  ee.initialize(null, null, function() {
+    // ... run analysis ...
+  }, function(e) {
+    console.error('Initialization error: ' + e);
+  });
+};
+
+// Authenticate using an OAuth pop-up.
+ee.data.authenticateViaOauth(YOUR_CLIENT_ID, initialize, function(e) {
+  console.error('Authentication error: ' + e);
+}, null, function() {
+  ee.data.authenticateViaPopup(initialize);
+});
